b412abe7aaf889c5fdd7291da360958e577feea2
1733492644 <1733492644@qq.com>
2016-05-05 14:01:19 +0800

    添加介绍图片

--- CSSlideMenu/ViewController.m
+++ CSSlideMenu/ViewController.m
@@ -18,10 +18,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //创建侧滑菜单
     CGRect frame = CGRectMake(-250, 0, 250, self.view.frame.size.height);
     CSSlideMenu *menu = [[CSSlideMenu alloc]initWithFrame:frame SuperView:self.view GesturesView:self.view];
     [self.view addSubview:menu];
     
+    //添加自己需要的内容UIView
     UIView *view = [[UIView alloc]init];
     view.backgroundColor = [UIColor redColor];
     view.frame = CGRectMake(15, 50, 100, 100);
